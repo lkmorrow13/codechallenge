51ecd42baa4a8deaf8628d0e70bde4ec32fec7bd
Laven Morrow <lmorrow13@live.com>
2018-02-02 11:51:14 -0700

    intial commit

---
+++ untitled.js
@@ -0,0 +1,10 @@
+// I'm guessing that i need to write a counting function that takes the input number and counts it. and with every count adds a 1 or 0 to the string.
+// So what i am thinking is that i need to create an array with 1 and 0 and have it count through that and each one it counts to the string.
+ 
+
+function stringy(size) {
+	this.size = ['1', '0'];
+	for (var i = 0; i < this.size.length; i++) {
+		this.size[i] 
+	}
+}
